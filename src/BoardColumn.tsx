f971ca12dbb044b65f3138f291935ae375ab7d22
Fabian Tan <ftan@polaris.senacor.com>
2022-02-05 23:54:15 +0100

    TASK-5 Add styling

--- src/BoardColumn.tsx
+++ src/BoardColumn.tsx
@@ -6,20 +6,21 @@ import {Item} from "./types";
 import {Droppable} from "react-beautiful-dnd";
 
 const Container = styled.div`
-    margin: 8px;
     border: 1px solid #AAA;
     border-radius: 2px;    
     background-color: #EFEFEF;
     padding: 2px;
-    min-width: 300px;
+    min-width: 490px;
     display: flex;
     flex-direction: column;
 `;
 const Title = styled.div`
-    padding: 8px;
+    padding: 5px;
+    text-align: left;
+    margin: 10px;
+    font-weight: bold;
 `;
-const NewsList = styled.h3`
-    padding: 8px;
+const NewsList = styled.div`    
     transition: background-color 0.2s ease;
     flex-grow: 1;
     min-height: 100px;
@@ -33,14 +34,19 @@ type BoardColumnProps = {
 function BoardColumn(props: BoardColumnProps) {
     return (
         <Container>
-            <Title>{props.column.title}</Title>`
+            <Title>{props.column.title} ({props.items.length})</Title>
             <Droppable droppableId={props.column.id} key={props.column.id}>
                 {(provided) => (
                     <NewsList
                         ref={provided.innerRef}
                         {...provided.droppableProps}
                     >
-                        {props.items.map((item, index) => <DraggableItem key={'' + item.id} item={item} index={index}/>)}
+                        {props.items.map((item, index) =>
+                                <DraggableItem
+                                    key={'' + item.id}
+                                    item={item}
+                                    index={index}/>
+                        )}
                         {provided.placeholder}
                     </NewsList>
                 )}
